22b5f71c85a164a59632bee30267f9518385de41
K-iise <kimyungi1004@naver.com>
2026-03-01 16:02:54 +0900

    feat: 예외 처리 추가

--- Pre-Blackjack/src/main/java/CalculateCard.java
+++ Pre-Blackjack/src/main/java/CalculateCard.java
@@ -6,12 +6,24 @@ public class CalculateCard {
         System.out.println(c.calculateStringCard("1"));
         System.out.println(c.calculateStringCard("J"));
         System.out.println(c.calculateStringCard("Q"));
+        //System.out.println(c.calculateStringCard("D"));
+        //System.out.println(c.calculateStringCard("11"));
+        System.out.println(c.calculateStringCard("-1"));
     }
 
     public int calculateStringCard(String card) {
         if (judgeCard(card))
             return 10;
-        return Integer.parseInt(card);
+
+        try {
+            int value = Integer.parseInt(card);
+            if (value < 1 || value > 10) {
+                throw new IllegalArgumentException("숫자 카드의 범위는 1 ~ 10 사이여야 합니다.");
+            }
+            return value;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("옳바르지 않은 카드 입니다 : " + card);
+        }
     }
 
     public boolean judgeCard(String card) {
